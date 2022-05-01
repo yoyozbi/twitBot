dcae5ff5d20db8add188e3f04deb785df776bb61
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2022-05-01 22:16:47 +0200

    moved webhook send to main and add a clean exit

--- src/main.go
+++ src/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/yoyozbi/twitBot/src/twitterImpl"
+	"github.com/yoyozbi/twitBot/src/utils"
+	"github.com/yoyozbi/twitBot/src/webhook"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/yoyozbi/twitBot/src/twitterImpl"
 )
 
 func main() {
+	config := utils.LoadConfig()
+
 	c := make(chan *twitter.Tweet)
 	go twitterImpl.Start(c)
-	ch := make(chan os.Signal,1)
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-ch
+		close(c)
+		os.Exit(1)
+	}()
+	for {
+		tweet := <-c
+		for _, t := range config.Track {
+			if t.Username == tweet.User.ScreenName {
+				err := webhook.Post(t, tweet)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+	}
+	/*signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	log.Println(<-ch)
-	close(c)
+	close(c)*/
 }
--- src/twitterImpl/main.go
+++ src/twitterImpl/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/yoyozbi/twitBot/src/utils"
-	"github.com/yoyozbi/twitBot/src/webhook"
 	"golang.org/x/oauth2/clientcredentials"
 )
 
@@ -16,50 +15,38 @@ func Start(c chan *twitter.Tweet) {
 	config := utils.LoadConfig()
 
 	twitConfig := &clientcredentials.Config{
-		ClientID:  config.ConsumerKey,
+		ClientID:     config.ConsumerKey,
 		ClientSecret: config.ConsumerSecret,
-		TokenURL: "https://api.twitter.com/oauth2/token",
+		TokenURL:     "https://api.twitter.com/oauth2/token",
 	}
-	httpClient := twitConfig.Client(context.Background())	
+	httpClient := twitConfig.Client(context.Background())
 	client := twitter.NewClient(httpClient)
 
 	st := &Stream{
 		client: httpClient,
-	}	
+	}
 
 	_, err := st.SetRules([]Rule{{Value: config.TwitterRule()}})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
 	r := make(chan StreamResponse)
 	go st.Connect(r)
 	defer close(r)
 	for {
 		m := <-r
-		iId, err := strconv.ParseInt(m.Data.Id,10,64)
+		iId, err := strconv.ParseInt(m.Data.Id, 10, 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		tweet, _, err := client.Statuses.Show(iId,&twitter.StatusShowParams{TrimUser:BoolPointer(false),IncludeEntities: BoolPointer(true)})
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _,t := range config.Track {
-			if t.Username == tweet.User.ScreenName {
-				err = webhook.Post(t, tweet)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-		}
+		tweet, _, err := client.Statuses.Show(iId, &twitter.StatusShowParams{TrimUser: BoolPointer(false), IncludeEntities: BoolPointer(true)})
 
-		c <- tweet	
+		c <- tweet
 	}
 
 }
 
 func BoolPointer(b bool) *bool {
-    return &b
+	return &b
 }
