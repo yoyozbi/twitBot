029437e631f31ca747217f8d258393904505b46a
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2022-06-12 14:25:27 +0200

    correct a bug that prevented streamRules from being deleted or created

--- src/twitterImpl/main.go
+++ src/twitterImpl/main.go
@@ -1,26 +1,22 @@
 package twitterImpl
 
 import (
-  "log"
 	"github.com/yoyozbi/twitBot/src/utils"
+	"log"
 )
 
 func Start(c chan *Tweet) {
 
 	config := utils.LoadConfig()
 
-  //oAuth1config := oauth1.NewConfig()
-	//twitConfig := &clientcredentials.Config{
-	//	ClientID:     config.ConsumerKey,
-	//	ClientSecret: config.ConsumerSecret,
-	//	TokenURL:     "https://api.twitter.com/oauth2/token",
-	//}
-	//client := twitter.NewClient(httpClient)
-  t := TwitterApi{
-    Config: config,
-  }	
-
-	_, err := t.SetStreamRules([]StreamRule{{Value: config.TwitterRule()}})
+	t := TwitterApi{
+		Config: config,
+	}
+	_, err := t.DeleteAllStreamRules()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = t.SetStreamRules([]StreamRule{{Value: config.TwitterRule()}})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,15 +27,11 @@ func Start(c chan *Tweet) {
 	for {
 		m := <-r
 
-    tweet, err := t.GetTweet(m.Data.Id)
-    if err != nil{
-      log.Fatal(err)
-    }
-    c <- &tweet
+		tweet, err := t.GetTweet(m.Data.Id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c <- &tweet
 	}
 
-}
-
-func BoolPointer(b bool) *bool {
-	return &b
 }
